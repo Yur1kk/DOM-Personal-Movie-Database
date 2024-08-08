e13b9c7bcf29106cf88aa6d25b21836e781b4335
Yurii Soroka <urasor04@gmail.com>
2024-08-09 02:25:29 +0300

    Added parts of code that allows to interact with backdrop window, provided work of cancel button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,26 @@
-const addMovieModal = document.getElementById('add-modal');
-const startMovieButton = document.querySelector('header button');
+const addMovieModal = document.getElementById("add-modal");
+const startAddMovieButton = document.querySelector("header button");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 
-const toggleMovieModal = () => {
-  addMovieModal.classList.toggle('visible');
+const backdrop = document.getElementById("backdrop");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
 };
 
-startMovieButton.addEventListener('click', toggleMovieModal);
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
